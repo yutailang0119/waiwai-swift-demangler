906fa67c9dee5dbe080df6ae93b0f8131de7f811
yutailang0119 <muta.yutaro@gmail.com>
2018-12-16 10:34:35 +0900

    Step1: Add isSwiftSymbol(name:) and isFunctionEntitySpec(name:)

--- Sources/SwiftDemangler/SwiftDemangler.swift
+++ Sources/SwiftDemangler/SwiftDemangler.swift
@@ -1,3 +1,11 @@
 public func demangle(name: String) -> String {
     return name //TODO: implement
 }
+
+internal func isSwiftSymbol(name: String) -> Bool {
+    return name.hasPrefix("$S")
+}
+
+internal func isFunctionEntitySpec(name: String) -> Bool {
+    return name.hasSuffix("F")
+}
